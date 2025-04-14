914c48492c86a6119b21cbfabe2ed8d486b5d523
normaldeve <junnukim1007@gmail.com>
2025-04-14 17:09:18 +0900

    통합 테스트 유저 일부 작성 완료

--- src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
+++ src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
@@ -1,43 +1,44 @@
-package com.sprint.mission.discodeit.controller;
+package com.sprint.mission.discodeit.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.UserStatusService;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.transaction.annotation.Transactional;
 
+@SpringBootTest
 @ActiveProfiles("test")
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
-
+@AutoConfigureMockMvc
+@Transactional
+public class UserIntegrationTest {
   @Autowired
   private MockMvc mockMvc;
-
-  @MockitoBean
+  @Autowired
   private UserService userService;
-
-  @MockitoBean
-  private UserStatusService userStatusService;
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Test
   @DisplayName("사용자 생성 요청 성공 - 프로필 포함 + JSON 응답 검증")
@@ -45,20 +46,14 @@ public class UserControllerTest {
     UserCreateRequest createRequest = new UserCreateRequest(
         "junwo",
         "junwo@email.com",
-        "Password1!"
+        "Password123!"
     );
 
     MockMultipartFile userCreateRequest = new MockMultipartFile(
         "userCreateRequest",
-        "",
-        "application/json",
-        """
-        {
-          "username": "junwo",
-          "email": "junwo@email.com",
-          "password": "Password123!"
-        }
-        """.getBytes(StandardCharsets.UTF_8)
+        "userCreateRequest.json",
+        MediaType.APPLICATION_JSON_VALUE,
+        objectMapper.writeValueAsBytes(createRequest)
     );
 
     MockMultipartFile profileFile = new MockMultipartFile(
@@ -68,23 +63,16 @@ public class UserControllerTest {
         "fake-image-bytes".getBytes(StandardCharsets.UTF_8)
     );
 
-    UUID userId = UUID.randomUUID();
-    BinaryContentDto profile = new BinaryContentDto(UUID.randomUUID(), "profile.png", 10L,
-        MediaType.IMAGE_JPEG_VALUE);
-    UserDto userDto = new UserDto(userId, "junwo", "junwo@email.com", profile, false);
-
-    when(userService.create(any(), any())).thenReturn(userDto);
-
     mockMvc.perform(multipart("/api/users")
             .file(userCreateRequest)
             .file(profileFile)
             .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
         .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").value(userId.toString()))
+        .andExpect(jsonPath("$.id", notNullValue()))
         .andExpect(jsonPath("$.username").value("junwo"))
         .andExpect(jsonPath("$.email").value("junwo@email.com"))
         .andExpect(jsonPath("$.profile.fileName").value("profile.png"))
-        .andExpect(jsonPath("$.online").value(false));
+        .andExpect(jsonPath("$.online").value(true));
   }
 
   @Test
@@ -136,30 +124,22 @@ public class UserControllerTest {
   }
 
   @Test
-  @DisplayName("사용자 전체 조회 - JSON 응답 검증")
+  @DisplayName("모든 사용자 조회 통합 테스트")
   void findAllUsers_success() throws Exception {
-    UUID userId = UUID.randomUUID();
-    when(userService.findAll()).thenReturn(List.of(
-        new UserDto(userId, "junwo", "junwo@email.com", null, null)
-    ));
+      // given
+    UserCreateRequest request1 = new UserCreateRequest("test1", "test1@naver.com", "Rlawnsdn12!");
+    UserCreateRequest request2 = new UserCreateRequest("test2", "test2@naver.com", "Rlawnsdn12!");
 
-    mockMvc.perform(get("/api/users"))
+    userService.create(request1, Optional.empty());
+    userService.create(request2, Optional.empty());
+
+    mockMvc.perform(get("/api/users")
+            .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$[0].id").value(userId.toString()))
-        .andExpect(jsonPath("$[0].username").value("junwo"))
-        .andExpect(jsonPath("$[0].email").value("junwo@email.com"))
-        .andExpect(jsonPath("$[0].profile").doesNotExist())
-        .andExpect(jsonPath("$[0].status").doesNotExist());
-  }
-
-  @Test
-  @DisplayName("사용자 삭제")
-  void deleteUser_success() throws Exception {
-    UUID userId = UUID.randomUUID();
-
-    mockMvc.perform(delete("/api/users/{userId}", userId))
-        .andExpect(status().isNoContent());
-
-    verify(userService).delete(userId);
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andExpect(jsonPath("$[0].username", is("test1")))
+        .andExpect(jsonPath("$[0].email", is("test1@naver.com")))
+        .andExpect(jsonPath("$[1].username", is("test2")))
+        .andExpect(jsonPath("$[1].email", is("test2@naver.com")));
   }
 }
